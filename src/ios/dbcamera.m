5cf97ebdee96086fd4f4667cee37cb47f8e8662b
mkcode <chrisewald@gmail.com>
2014-04-10 23:10:48 -0400

    plugman platform add ios

---
+++ src/ios/dbcamera.m
@@ -0,0 +1,28 @@
+/********* CDVdbcamera.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface CDVdbcamera : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation CDVdbcamera
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
